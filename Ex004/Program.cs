cab1e734cddbefc70c6967b7b93471bf0819973a
alexandravdeenko <alex-avdeenko@yandex.ru>
2022-09-21 11:34:20 +0700

    Принимает на вход семь чисел, и находит их среднее арифметическое

---
+++ Ex004/Program.cs
@@ -0,0 +1,16 @@
+﻿int count = 7;
+int[] array = new int[count];
+int i = 0;
+double sum = 0;
+Console.WriteLine("Числа: ");
+
+while (i < count)
+{
+    array[i] = new Random().Next(1, 100);
+    Console.WriteLine(array[i]);
+    sum = sum + array[i];
+    i++;
+}
+
+double m = sum / count;
+Console.WriteLine("Среднее арифметическое " + m);
