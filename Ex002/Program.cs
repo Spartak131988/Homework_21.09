aa09d9f238aa7f2036729f5eebbe045400b65c90
alexandravdeenko <alex-avdeenko@yandex.ru>
2022-09-21 11:25:08 +0700

    Выводит третью цифру заданного числа или сообщает, что третьей цифры нет

---
+++ Ex002/Program.cs
@@ -0,0 +1,12 @@
+﻿int number = 645;
+Console.WriteLine("Число: " + number);
+
+if (number < 100)
+{
+    Console.WriteLine("Нет третьей цифры");
+}
+else
+{
+    string str = number.ToString();
+    Console.Write("Третья цифра числа: " + str[2]);
+}
