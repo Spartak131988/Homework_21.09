1b70f0fa3c54e3e1bb8ec6ae2344f1d639b3235a
alexandravdeenko <alex-avdeenko@yandex.ru>
2022-09-21 11:30:12 +0700

    Принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным или рабочим

---
+++ Ex003/Program.cs
@@ -0,0 +1,12 @@
+﻿int days = new Random().Next(1, 8);
+Console.Write("Число обознащающее день недели ");
+Console.WriteLine(days);
+
+if (days <= 5)
+{
+    Console.WriteLine("Рабочий день недели");
+}
+else
+{
+    Console.WriteLine("Выходной день недели");
+}
